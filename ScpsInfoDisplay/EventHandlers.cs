36a8efd35391031198c9d61e45ed401e39254246
bladuk <68610520+bladuk@users.noreply.github.com>
2022-06-12 16:01:07 +0300

    Code cleaning

--- ScpsInfoDisplay/EventHandlers.cs
+++ ScpsInfoDisplay/EventHandlers.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using Exiled.API.Extensions;
 using Exiled.API.Features;
+using Exiled.API.Features.Roles;
 using Exiled.Events.EventArgs;
 using MEC;
 using UnityEngine;
@@ -13,57 +14,28 @@ namespace ScpsInfoDisplay
     {
         private readonly Dictionary<Player, CoroutineHandle> _allDisplays = new Dictionary<Player, CoroutineHandle>();
 
-        public void OnWaitingForPlayers()
-        {
-            if (ScpsInfoDisplay.Singleton.Config.TextAlignment.ToLower() != "center" && ScpsInfoDisplay.Singleton.Config.TextAlignment.ToLower() != "left" && ScpsInfoDisplay.Singleton.Config.TextAlignment.ToLower() != "right")
-            {
-                Log.Warn(ScpsInfoDisplay.Singleton.Config.TextAlignment + " is an invalid value of text_alignment config. It will be replaced with the default value (right).");
-                ScpsInfoDisplay.Singleton.Config.TextAlignment = "right";
-            }
-
-            if (ScpsInfoDisplay.Singleton.Config.MarkPlayerInList && string.IsNullOrEmpty(ScpsInfoDisplay.Singleton.Config.PlayersMarker))
-            {
-                Log.Warn("Player's marker string is null or empty. It will be replaced with the default value (You -->).");
-                ScpsInfoDisplay.Singleton.Config.PlayersMarker = "<color=#D51D1D>You --></color>";
-            }
-
-            if (ScpsInfoDisplay.Singleton.Config.TextPositionOffset < -10)
-            {
-                Log.Warn("Text position offset can't be less than -10.");
-                ScpsInfoDisplay.Singleton.Config.TextPositionOffset = -10;
-            }
-
-            if (ScpsInfoDisplay.Singleton.Config.TextPositionOffset > 42)
-            {
-                Log.Warn("Text position offset can't be more than 42.");
-                ScpsInfoDisplay.Singleton.Config.TextPositionOffset = 42;
-            }
-        }
+        public void OnReloadedConfigs() => ScpsInfoDisplay.Singleton.ValidateConfigs();
 
         public void OnRoundRestart()
         {
-            foreach (KeyValuePair<Player, CoroutineHandle> display in _allDisplays.ToList())
+            foreach (var display in _allDisplays.Where(display => display.Key != null && display.Value.IsRunning))
             {
-                if (display.Key != null && display.Value.IsRunning)
-                    Timing.KillCoroutines(display.Value);
-                _allDisplays.Remove(display.Key);
+                Timing.KillCoroutines(display.Value);
             }
             _allDisplays.Clear();
         }
 
         public void OnPlayerChangingRole(ChangingRoleEventArgs ev)
         {
-            if (ev.Player != null)
+            if (ev.Player == null) return;
+            if (_allDisplays.ContainsKey(ev.Player) && !ScpsInfoDisplay.Singleton.Config.DisplayStrings.ContainsKey(ev.NewRole))
             {
-                if (_allDisplays.ContainsKey(ev.Player) && !ScpsInfoDisplay.Singleton.Config.DisplayStrings.ContainsKey(ev.NewRole))
-                {
-                    Timing.KillCoroutines(_allDisplays[ev.Player]);
-                    _allDisplays.Remove(ev.Player);
-                }
-                else if ((!_allDisplays.ContainsKey(ev.Player) && ev.NewRole.GetTeam() == Team.SCP && ScpsInfoDisplay.Singleton.Config.DisplayStrings.ContainsKey(ev.NewRole)) || ScpsInfoDisplay.Singleton.Config.CustomRolesIntegrations.Keys.Any(key => ev.Player.SessionVariables.ContainsKey(key)))
-                {
-                    _allDisplays.Add(ev.Player, Timing.RunCoroutine(_showDisplay(ev.Player)));
-                }
+                Timing.KillCoroutines(_allDisplays[ev.Player]);
+                _allDisplays.Remove(ev.Player);
+            }
+            if (!_allDisplays.ContainsKey(ev.Player) && ((ev.NewRole.GetTeam() == Team.SCP && ScpsInfoDisplay.Singleton.Config.DisplayStrings.ContainsKey(ev.NewRole)) || ScpsInfoDisplay.Singleton.Config.CustomRolesIntegrations.Keys.Any(key => ev.Player.SessionVariables.ContainsKey(key))))
+            {
+                _allDisplays.Add(ev.Player, Timing.RunCoroutine(_showDisplay(ev.Player)));
             }
         }
 
@@ -71,36 +43,27 @@ namespace ScpsInfoDisplay
         {
             for (; ; )
             {
-                yield return Timing.WaitForSeconds(0.5f);
+                yield return Timing.WaitForSeconds(1f);
                 try
                 {
                     if (player != null)
                     {
                         string text = string.Empty;
-                        foreach (KeyValuePair<string, string> integration in ScpsInfoDisplay.Singleton.Config.CustomRolesIntegrations)
+                        foreach (var integration in ScpsInfoDisplay.Singleton.Config.CustomRolesIntegrations)
                         {
-                            foreach (Player any in Player.List)
-                            {
-                                if (any != null && any.SessionVariables.ContainsKey(integration.Key))
-                                {
-                                    text += $"<align={ScpsInfoDisplay.Singleton.Config.TextAlignment}>" + (player == any && ScpsInfoDisplay.Singleton.Config.MarkPlayerInList ? ScpsInfoDisplay.Singleton.Config.PlayersMarker + " " : "") + integration.Value.Replace("%arhealth%", any.ArtificialHealth > 0 ? any.ArtificialHealth.ToString() : "").Replace("%healthpercent%", Math.Round((Mathf.Clamp01(any.Health / (float)any.MaxHealth) * 100), 0).ToString()).Replace("%health%", Math.Round(any.Health, 0).ToString()).Replace("%generators%", Generator.List.Count(gen => gen.IsEngaged).ToString()).Replace("%engaging%", Generator.List.Count(gen => gen.IsActivating) > 0 ? $" (+{Generator.List.Count(gen => gen.IsActivating)})" : "").Replace("%zombies%", Player.List.Count(p => p.Role.Type == RoleType.Scp0492).ToString()).Replace("%distance%", any != player ? Math.Round(Vector3.Distance(player.Position, any.Position), 0).ToString() + "m" : "") + "</align>\n";
-                                }
-                            }
+                            text = Player.List.Where(p => p?.SessionVariables.ContainsKey(integration.Key) == true).Aggregate(text, (current, any) => current + $"<align={ScpsInfoDisplay.Singleton.Config.TextAlignment}>" + (player == any && ScpsInfoDisplay.Singleton.Config.MarkPlayerInList ? ScpsInfoDisplay.Singleton.Config.PlayersMarker + " " : "") + ProcessStringVariables(integration.Value, player, any) + "</align>\n");
                         }
 
-                        foreach (Player scp in Player.List.Where(p => p.Role.Team == Team.SCP && ScpsInfoDisplay.Singleton.Config.DisplayStrings.ContainsKey(p.Role.Type)))
+                        foreach (var scp in Player.List.Where(p => p?.Role.Team == Team.SCP && ScpsInfoDisplay.Singleton.Config.DisplayStrings.ContainsKey(p.Role.Type)))
                         {
-                            if (scp != null)
-                            {
-                                text += $"<align={ScpsInfoDisplay.Singleton.Config.TextAlignment}>" + (player == scp && ScpsInfoDisplay.Singleton.Config.MarkPlayerInList ? ScpsInfoDisplay.Singleton.Config.PlayersMarker + " " : "") + ScpsInfoDisplay.Singleton.Config.DisplayStrings[scp.Role.Type].Replace("%arhealth%", scp.ArtificialHealth > 0 ? scp.ArtificialHealth.ToString() : "").Replace("%healthpercent%", Math.Round((Mathf.Clamp01(scp.Health / (float)scp.MaxHealth) * 100), 0).ToString()).Replace("%health%", Math.Round(scp.Health, 0).ToString()).Replace("%generators%", Generator.List.Count(gen => gen.IsEngaged).ToString()).Replace("%engaging%", Generator.List.Count(gen => gen.IsActivating) > 0 ? $" (+{Generator.List.Count(gen => gen.IsActivating)})" : "").Replace("%zombies%", Player.List.Count(p => p.Role.Type == RoleType.Scp0492).ToString()).Replace("%distance%", scp != player ? Math.Round(Vector3.Distance(player.Position, scp.Position), 0).ToString() + "m" : "") + "</align>\n";
-                            }
+                            text += $"<align={ScpsInfoDisplay.Singleton.Config.TextAlignment}>" + (player == scp && ScpsInfoDisplay.Singleton.Config.MarkPlayerInList ? ScpsInfoDisplay.Singleton.Config.PlayersMarker + " " : "") + ProcessStringVariables(ScpsInfoDisplay.Singleton.Config.DisplayStrings[scp.Role.Type], player, scp) + "</align>\n";
                         }
 
-                        if (ScpsInfoDisplay.Singleton.Config.TextPositionOffset >= 0 || (ScpsInfoDisplay.Singleton.Config.TextPositionOffset < 0 && (ScpsInfoDisplay.Singleton.Config.TextPositionOffset + text.Count(t => t == '\n')) > 0))
+                        if (ScpsInfoDisplay.Singleton.Config.TextPositionOffset >= 0 || (ScpsInfoDisplay.Singleton.Config.TextPositionOffset < 0 && ScpsInfoDisplay.Singleton.Config.TextPositionOffset + text.Count(t => t == '\n') > 0))
                             text += new string('\n', ScpsInfoDisplay.Singleton.Config.TextPositionOffset >= 0 ? Math.Abs(ScpsInfoDisplay.Singleton.Config.TextPositionOffset - text.Count(t => t == '\n')) : ScpsInfoDisplay.Singleton.Config.TextPositionOffset + text.Count(t => t == '\n'));
                         else if (ScpsInfoDisplay.Singleton.Config.TextPositionOffset < 0)
                             text = text.Insert(0, new string('\n', Math.Abs(ScpsInfoDisplay.Singleton.Config.TextPositionOffset + text.Count(t => t == '\n'))));
-                        player.ShowHint(text, 1f);
+                        player.ShowHint(text, 1.1f);
                     }
                 } 
                 catch (Exception ex)
@@ -109,5 +72,16 @@ namespace ScpsInfoDisplay
                 }
             }
         }
+
+        private string ProcessStringVariables(string raw, Player observer, Player target) => raw
+            .Replace("%arhealth%", target.ArtificialHealth > 0 ? target.ArtificialHealth.ToString() : "")
+            .Replace("%healthpercent%", Math.Floor(Mathf.Clamp01(target.Health / target.MaxHealth) * 100).ToString())
+            .Replace("%health%", Math.Floor(target.Health).ToString())
+            .Replace("%generators%", Generator.List.Count(gen => gen.IsEngaged).ToString())
+            .Replace("%engaging%", Generator.List.Count(gen => gen.IsActivating) > 0 ? $" (+{Generator.List.Count(gen => gen.IsActivating)})" : "").Replace("%zombies%", Player.List.Count(p => p.Role.Type == RoleType.Scp0492).ToString())
+            .Replace("%distance%", target != observer ? Math.Floor(Vector3.Distance(observer.Position, target.Position)) + "m" : "")
+            .Replace("%079level%", target.Role.Is(out Scp079Role _) ? (target.Role.As<Scp079Role>().Level + 1).ToString() : "")
+            .Replace("%079energy%", target.Role.Is(out Scp079Role _) ? Math.Floor(target.Role.As<Scp079Role>().Energy).ToString() : "")
+            .Replace("%079experience%", target.Role.Is(out Scp079Role _) ? Math.Floor(target.Role.As<Scp079Role>().Experience).ToString() : "");
     }
 }
--- ScpsInfoDisplay/ScpsInfoDisplay.cs
+++ ScpsInfoDisplay/ScpsInfoDisplay.cs
@@ -10,7 +10,7 @@ namespace ScpsInfoDisplay
         public override string Prefix => "scpsinfodisplay";
         public override string Name => "ScpsInfoDisplay";
         public override string Author => "bladuk.";
-        public override Version Version { get; } = new Version(1, 1, 2);
+        public override Version Version { get; } = new Version(1, 1, 5);
         public override Version RequiredExiledVersion { get; } = new Version(5, 2, 1);
         public static ScpsInfoDisplay Singleton = new ScpsInfoDisplay();
         private EventHandlers _eventHandlers;
@@ -19,6 +19,8 @@ namespace ScpsInfoDisplay
         {
             Singleton = this;
             _eventHandlers = new EventHandlers();
+
+            ValidateConfigs();
             RegisterEvents();
 
             base.OnEnabled();
@@ -27,20 +29,48 @@ namespace ScpsInfoDisplay
         public override void OnDisabled()
         {
             UnregisterEvents();
+            _eventHandlers = null;
 
             base.OnDisabled();
         }
 
+        internal void ValidateConfigs()
+        {
+            if (!string.Equals(Config.TextAlignment, "center", StringComparison.OrdinalIgnoreCase) && !string.Equals(Config.TextAlignment, "left", StringComparison.OrdinalIgnoreCase) && !string.Equals(Config.TextAlignment, "right", StringComparison.OrdinalIgnoreCase))
+            {
+                Log.Warn(Config.TextAlignment + " is an invalid value of text_alignment config. It will be replaced with the default value (right).");
+                Config.TextAlignment = "right";
+            }
+
+            if (Config.MarkPlayerInList && string.IsNullOrEmpty(Config.PlayersMarker))
+            {
+                Log.Warn("Player's marker string is null or empty. It will be replaced with the default value (You -->).");
+                Config.PlayersMarker = "<color=#D51D1D>You --></color>";
+            }
+
+            if (Config.TextPositionOffset < -10)
+            {
+                Log.Warn("Text position offset can't be less than -10.");
+                Config.TextPositionOffset = -10;
+            }
+
+            if (Config.TextPositionOffset > 42)
+            {
+                Log.Warn("Text position offset can't be more than 42.");
+                Config.TextPositionOffset = 42;
+            }
+        }
+
         private void RegisterEvents()
         {
-            Server.WaitingForPlayers += _eventHandlers.OnWaitingForPlayers;
+            Server.ReloadedConfigs += _eventHandlers.OnReloadedConfigs;
             Server.RestartingRound += _eventHandlers.OnRoundRestart;
             Player.ChangingRole += _eventHandlers.OnPlayerChangingRole;
         }
 
         private void UnregisterEvents()
         {
-            Server.WaitingForPlayers -= _eventHandlers.OnWaitingForPlayers;
+            Server.ReloadedConfigs -= _eventHandlers.OnReloadedConfigs;
             Server.RestartingRound -= _eventHandlers.OnRoundRestart;
             Player.ChangingRole -= _eventHandlers.OnPlayerChangingRole;
         }
