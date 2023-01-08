ce432ab1b6054f0ec0133edc57597313477ed1ed
bladuk <68610520+bladuk@users.noreply.github.com>
2023-01-08 23:11:53 +0300

    NWAPI version

---
+++ ScpsInfoDisplay-NWAPI/EventHandlers.cs
@@ -0,0 +1,96 @@
+﻿using PluginAPI.Enums;
+using PlayerRoles;
+using PluginAPI.Core;
+using PluginAPI.Core.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using MapGeneration.Distributors;
+using UnityEngine;
+using MEC;
+using PlayerRoles.PlayableScps.Scp079;
+using PlayerRoles.PlayableScps.Scp106;
+using Object = UnityEngine.Object;
+
+namespace ScpsInfoDisplay
+{
+    public class EventHandlers
+    {
+        private CoroutineHandle _displayCoroutine;
+        private List<Scp079Generator> _generators = new List<Scp079Generator>();
+        
+        [PluginEvent(ServerEventType.WaitingForPlayers)]
+        public void OnWaitingForPlayers()
+        {
+            _generators = Object.FindObjectsOfType<Scp079Generator>().ToList();
+        }
+
+        [PluginEvent(ServerEventType.RoundStart)]
+        public void OnRoundStart()
+        {
+            if (_displayCoroutine.IsRunning)
+                Timing.KillCoroutines(_displayCoroutine);
+
+            _displayCoroutine = Timing.RunCoroutine(ShowDisplay());
+        }
+
+        private IEnumerator<float> ShowDisplay()
+        {
+            while (!RoundSummary.singleton._roundEnded)
+            {
+                yield return Timing.WaitForSeconds(1f);
+                try
+                {
+                    foreach (var player in Player.GetPlayers().Where(p => p != null && (ScpsInfoDisplay.Singleton.Config.DisplayStrings.ContainsKey(p.Role) || ScpsInfoDisplay.Singleton.Config.CustomRolesIntegrations.Keys.Any(key => p.TemporaryData.Contains(key)))))
+                    {
+                        string text = $"<align={ScpsInfoDisplay.Singleton.Config.TextAlignment}>";
+                        
+                        foreach (var integration in ScpsInfoDisplay.Singleton.Config.CustomRolesIntegrations)
+                        {
+                            text = Player.GetPlayers().Where(p => p?.TemporaryData.Contains(integration.Key) == true).Aggregate(text, (current, any) => current + (player == any && ScpsInfoDisplay.Singleton.Config.MarkPlayerInList ? ScpsInfoDisplay.Singleton.Config.PlayersMarker + " " : "") + ProcessStringVariables(integration.Value, player, any) + "\n");
+                        }
+
+                        foreach (var scp in Player.GetPlayers().Where(p => p?.Role.GetTeam() == Team.SCPs && ScpsInfoDisplay.Singleton.Config.DisplayStrings.ContainsKey(p.Role)))
+                        {
+                            text += (player == scp && ScpsInfoDisplay.Singleton.Config.MarkPlayerInList ? ScpsInfoDisplay.Singleton.Config.PlayersMarker + " " : "") + ProcessStringVariables(ScpsInfoDisplay.Singleton.Config.DisplayStrings[scp.Role], player, scp) + "\n";
+                        }
+
+                        text += $"<voffset={ScpsInfoDisplay.Singleton.Config.TextPositionOffset}em> </voffset></align>";
+                        player.ReceiveHint(text, 1.25f);
+                    }
+                } 
+                catch (Exception ex)
+                {
+                    Log.Error(ex.ToString());
+                }
+            }
+        }
+
+        private string ProcessStringVariables(string raw, Player observer, Player target)
+        {
+            switch (target.ReferenceHub.roleManager.CurrentRole)
+            {
+                case Scp079Role scp079:
+                    scp079.SubroutineModule.TryGetSubroutine(out Scp079AuxManager aux);
+                    scp079.SubroutineModule.TryGetSubroutine(out Scp079TierManager tier);
+                    raw = raw
+                        .Replace("%079energy%", Math.Floor(aux.CurrentAux).ToString())
+                        .Replace("%079level%", tier.AccessTierLevel.ToString())
+                        .Replace("%079experience", tier.RelativeExp.ToString());
+                    break;
+                case Scp106Role scp106:
+                    scp106.SubroutineModule.TryGetSubroutine(out Scp106Vigor vigor);
+                    raw = raw.Replace("%106vigor%", Math.Floor(vigor.VigorAmount * 100).ToString());
+                    break;
+
+            }
+
+            return raw
+                .Replace("%healthpercent%", Math.Floor(target.Health / target.MaxHealth * 100).ToString())
+                .Replace("%health%", Math.Floor(target.Health).ToString())
+                .Replace("%generators%", _generators.Count(gen => gen.Engaged).ToString())
+                .Replace("%engaging%", _generators.Count(gen => gen.Activating) > 0 ? $" (+{_generators.Count(gen => gen.Activating)})" : "").Replace("%zombies%", Player.GetPlayers<Player>().Count(p => p.Role == RoleTypeId.Scp0492).ToString())
+                .Replace("%distance%", target != observer ? Math.Floor(Vector3.Distance(observer.Position, target.Position)) + "m" : "");
+        }
+    }
+}
---
+++ ScpsInfoDisplay-NWAPI/ScpsInfoDisplay.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using PluginAPI.Core;
+using PluginAPI.Core.Attributes;
+using PluginAPI.Events;
+
+namespace ScpsInfoDisplay
+{
+    internal sealed class ScpsInfoDisplay
+    {
+        public static ScpsInfoDisplay Singleton;
+
+        [PluginConfig]
+        public Config Config;
+
+        [PluginEntryPoint("ScpsInfoDisplay", "2.0.0", "A plugin that displays information about SCPs in player's team.", "bladuk.")]
+        private void LoadPlugin()
+        {
+            Singleton = this;
+            
+            #region Config validation
+            if (!string.Equals(Config.TextAlignment, "center", StringComparison.OrdinalIgnoreCase) && !string.Equals(Config.TextAlignment, "left", StringComparison.OrdinalIgnoreCase) && !string.Equals(Config.TextAlignment, "right", StringComparison.OrdinalIgnoreCase))
+            {
+                Log.Warning(Config.TextAlignment + " is an invalid value of text_alignment config. It will be replaced with the default value (right).");
+                Config.TextAlignment = "right";
+            }
+
+            if (Config.MarkPlayerInList && string.IsNullOrEmpty(Config.PlayersMarker))
+            {
+                Log.Warning("Player's marker string is null or empty. It will be replaced with the default value (You -->).");
+                Config.PlayersMarker = "<color=#D51D1D>You --></color>";
+            }
+            #endregion
+            
+            EventManager.RegisterEvents<EventHandlers>(this);
+        }
+    }
+}
