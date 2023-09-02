3badc27fcb5c2e27f0843e57f073aac6fa819f5b
Nikki Gardiner <mill385@yahoo.com>
2023-09-02 14:53:35 -0400

    Exiled Only Update to 8

--- ScpsInfoDisplay/Config.cs
+++ ScpsInfoDisplay/Config.cs
@@ -13,12 +13,12 @@ namespace ScpsInfoDisplay
         [Description("Display strings. Format: Role, display string.")]
         public Dictionary<RoleTypeId, string> DisplayStrings { get; set; } = new Dictionary<RoleTypeId, string>()
         {
-            { RoleTypeId.Scp049, "<color=#D51D1D>SCP-049 [%healthpercent%% Zombies: %zombies%] %distance%</color>" },
-            { RoleTypeId.Scp079, "<color=#D51D1D>SCP-079 [%generators%%engaging%/3] Lvl: %079level% Exp: %079experience% Energy: %079energy%</color>" },
-            { RoleTypeId.Scp096, "<color=#D51D1D>SCP-096 [%healthpercent%%] %distance%</color>" },
-            { RoleTypeId.Scp106, "<color=#D51D1D>SCP-106 [%healthpercent%%] Vigor: %106vigor%% %distance%</color>" },
-            { RoleTypeId.Scp173, "<color=#D51D1D>SCP-173 [%healthpercent%%] %distance%</color>" },
-            { RoleTypeId.Scp939, "<color=#D51D1D>SCP-939 [%healthpercent%%] %distance%</color>" },
+            { RoleTypeId.Scp049, "<color=#FF0000>SCP-049 [%healthpercent%% Zombies: %zombies%] </color>" },
+            { RoleTypeId.Scp079, "<color=#FF00000>SCP-079 [%generators%%engaging%/3] Lvl: %079level% Exp: %079experience% Energy: %079energy%</color>" },
+            { RoleTypeId.Scp096, "<color=#FF0000>SCP-096 [%healthpercent%%] </color>" },
+            { RoleTypeId.Scp106, "<color=#FF0000>SCP-106 [%healthpercent%%] Vigor: %106vigor%% </color>" },
+            { RoleTypeId.Scp173, "<color=#FF0000>SCP-173 [%healthpercent%%] </color>" },
+            { RoleTypeId.Scp939, "<color=#FF0000>SCP-939 [%healthpercent%%] </color>" },
         };
         [Description("Custom roles integrations. Format: SessionVariable that marks that the player belongs to that role, display string.")]
         public Dictionary<string, string> CustomRolesIntegrations { get; set; } = new Dictionary<string, string>();
@@ -27,7 +27,7 @@ namespace ScpsInfoDisplay
         [Description("Display text position offset.")]
         public sbyte TextPositionOffset { get; set; } = 30;
         [Description("The player seeing the list will be highlighted with the special marker to the left. Leave it empty if disabled.")]
-        public string PlayersMarker { get; set; } = "<color=#D51D1D>You --></color>";
+        public string PlayersMarker { get; set; } = "<color=#D7E607>You:</color>";
         [Description("Display debug messages in server console?")]
         public bool Debug { get; set; } = false;
     }
