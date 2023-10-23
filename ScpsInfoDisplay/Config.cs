862c92ac8f01e3ec2d82e7bca8267f1ba8013322
Snivy FIlms <120346554+NikkiGardiner1@users.noreply.github.com>
2023-10-23 10:22:27 -0400

    Added fancy formatting so its not just RED anymore

--- ScpsInfoDisplay/Config.cs
+++ ScpsInfoDisplay/Config.cs
@@ -13,12 +13,12 @@ namespace ScpsInfoDisplay
         [Description("Display strings. Format: Role, display string.")]
         public Dictionary<RoleTypeId, string> DisplayStrings { get; set; } = new Dictionary<RoleTypeId, string>()
         {
-            { RoleTypeId.Scp049, "<color=#FF0000>SCP-049 [%healthpercent%% Zombies: %zombies%] </color>" },
-            { RoleTypeId.Scp079, "<color=#FF00000>SCP-079 [%generators%%engaging%/3] Lvl: %079level% Exp: %079experience% Energy: %079energy%</color>" },
-            { RoleTypeId.Scp096, "<color=#FF0000>SCP-096 [%healthpercent%%] </color>" },
-            { RoleTypeId.Scp106, "<color=#FF0000>SCP-106 [%healthpercent%%] Vigor: %106vigor%% </color>" },
-            { RoleTypeId.Scp173, "<color=#FF0000>SCP-173 [%healthpercent%%] </color>" },
-            { RoleTypeId.Scp939, "<color=#FF0000>SCP-939 [%healthpercent%%] </color>" },
+            { RoleTypeId.Scp049, "<color=#FF0000><size=30>SCP-049</color> [<color=#19FF40>%healthpercent%%</color>]\n<color=#19B2FF>Zombies: %zombies%</color></size>" },
+            { RoleTypeId.Scp079, "<color=#FF0000><size=30>SCP-079</color> <color=#19FF40>Generators:</color> [<color=#19FF40>%generators%</color><color=#FF0000>%engaging%</color><color=#19FF40>/3</color>]\n <color=#19B2FF>Level: %079level%</color> <color=#FF19FF>Energy: %079energy%</color></size>" },
+            { RoleTypeId.Scp096, "<color=#FF0000><size=30>SCP-096</color> [<color=#19FF40>%healthpercent%%</color>] </size>" },
+            { RoleTypeId.Scp106, "<color=#FF0000><size=30>SCP-106</color> [<color=#19FF40>%healthpercent%%</color>] <color=#19B2FF>Vigor: %106vigor%% </size></color>" },
+            { RoleTypeId.Scp173, "<color=#FF0000><size=30>SCP-173</color> [<color=#19FF40>%healthpercent%%</color>] </size>" },
+            { RoleTypeId.Scp939, "<color=#FF0000><size=30>SCP-939</color> [<color=#19FF40>%healthpercent%%</color>] </size>" },
         };
         [Description("Custom roles integrations. Format: SessionVariable that marks that the player belongs to that role, display string.")]
         public Dictionary<string, string> CustomRolesIntegrations { get; set; } = new Dictionary<string, string>();
@@ -27,7 +27,7 @@ namespace ScpsInfoDisplay
         [Description("Display text position offset.")]
         public sbyte TextPositionOffset { get; set; } = 30;
         [Description("The player seeing the list will be highlighted with the special marker to the left. Leave it empty if disabled.")]
-        public string PlayersMarker { get; set; } = "<color=#D7E607>You:</color>";
+        public string PlayersMarker { get; set; } = "<color=#D7E607><size=30>You: </size></color>";
         [Description("Display debug messages in server console?")]
         public bool Debug { get; set; } = false;
     }
